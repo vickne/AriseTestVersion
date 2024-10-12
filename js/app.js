042d7e8979c03f4d74f70beaa549b8f558b0aefb
vickne <30363517+vickne@users.noreply.github.com>
2024-10-12 15:45:45 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,25 +10,19 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-let slideIndex = 1;
-showSlides();
-
-function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-}
- 
 }
 setTimeout(showSlides, 3000); // Change image every 3 seconds
 function myFunction() {
